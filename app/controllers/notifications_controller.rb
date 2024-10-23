2f3c80831064660048d661e4dc70790b8f1504c7
Alylson Monteiro <alylsonmonteiro@lammp-it.com.br>
2024-10-23 14:40:23 -0300

    Remoção de trechos de código e código comentado

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,51 +1,23 @@
 class NotificationsController < ApplicationController
     def create
-        #user = get_user(params[:user_id])
-        task = get_task(params[:task_id])
-        #extracted_data = get_extracted_data(params[:task_id])
+        task = params[:task_id]
+        extracted_data = params[:notification_payload]
         
+        if extracted_data[:price].nil? || extracted_data[:brand].nil? || extracted_data[:model].nil?
+            render json: { error: 'Missing data in notification payload' }, status: :bad_request
+            return
+        end
+
         notification = Notification.new(
-            task_id: task[:id],
-            # task_url: params[:task_url],
-            # task_status: params[:task_status],
-            # user_id: params[:user_id],
-            # user_name: params[:user_name],
-            # user_email: params[:user_email],
-            # extracted_data: params[:extracted_data]
-            #message: "Usuário #{user[:name]} concluiu a tarefa #{task[:url]}. Dados extraídos: #{extracted_data}.",
-            #notification_type: params[:notification_type],
+            task_id: task,
+            extracted_data: "Preço #{extracted_data[:price]}. Marca #{extracted_data[:brand]}. Modelo: #{extracted_data[:model]}."
         )
       
         if notification.save
-            #send_notification(notification)
             render json: { message: "Notification created and sent successfully" }, status: :created
         else
             render json: notification.errors, status: :unprocessable_entity
         end
     end
-  
-    private
-  
-    # def get_user(user_id)
-    #     response = Faraday.get("http://auth_service/users/#{user_id}")
-    #     JSON.parse(response.body, symbolize_names: true)
-    # end
-    
-    def get_task(task_id)
-        response = Faraday.get("http://task_service:3000/tasks/#{task_id}")
-        JSON.parse(response.body, symbolize_names: true)
-    end
-    
-    # def get_extracted_data(task_id)
-    #     response = Faraday.get("http://scraping_service/tasks/#{task_id}/data")
-    #     JSON.parse(response.body, symbolize_names: true)[:extracted_data]
-    # end
-  
-    # def send_notification(notification)
-    #     case notification.notification_type
-    #     when 'push'
-    #         # Lógica para enviar push notification
-    #     end
-    # end
 end
   
